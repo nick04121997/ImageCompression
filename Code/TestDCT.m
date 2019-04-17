f200e690d7f9f2b43b5a730865f8cc8bfc285925
Tianyi <tma@g.hmc.edu>
2019-04-16 20:24:51 -0700

    Used the Matlab DCT compression example on our image

--- Code/TestDCT.m
+++ Code/TestDCT.m
@@ -1,3 +1,29 @@
 img = imread('../Images/classic/mandril_gray.tif');
-img = im2double(img);
-T = dm_dct(img);
+I = im2double(img);
+
+T = dctmtx(8);
+dct = @(block_struct) T * block_struct.data * T';
+B = blockproc(I,[8 8],dct);
+
+mask = [1   1   1   1   0   0   0   0
+        1   1   1   0   0   0   0   0
+        1   1   0   0   0   0   0   0
+        1   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0
+        0   0   0   0   0   0   0   0];
+    
+% mask = zeros(8);
+% mask(1,1) = 1;
+        
+B2 = blockproc(B,[8 8],@(block_struct) mask .* block_struct.data);
+invdct = @(block_struct) T' * block_struct.data * T;
+I2 = blockproc(B2,[8 8],invdct);
+I_diff = abs(I2-I);
+
+imshow(I)
+figure;
+imshow(I2)
+figure;
+imshow(I_diff,[])
