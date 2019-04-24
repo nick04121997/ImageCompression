53ac0bc2718535dc56573345fc17abbfe9f1982d
Tianyi <tma@g.hmc.edu>
2019-04-24 16:59:17 -0700

    Fixed compression ratio

--- Code/evaluation.m
+++ Code/evaluation.m
@@ -3,15 +3,14 @@ close all
 img = imread('../Images/classic/mandril_gray.tif');
 [nzr_vector, psnr_vector, err_vector] = SVD_compression(img);
 
-
 figure;
 title("Error plot");
-plot(1:size(err_vector), err_vector);
-xlabel('Number of singular values');
+plot(nzr_vector, err_vector);
+xlabel('Compression ratio');
 ylabel('Average pixel error');
 
 figure;
 title("PSNR plot");
-plot(1:size(psnr_vector), psnr_vector);
-xlabel('Number of singular values');
+plot(nzr_vector, psnr_vector);
+xlabel('Compression ratio');
 ylabel('PSNR');
